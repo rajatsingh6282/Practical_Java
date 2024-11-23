fe516bf1eefbf40cfe96e778e8ace319970eab68
rajatsingh6282 <rajatsingh6282@gmail.com>
2024-11-23 10:37:42 +0530

    some minor changes about multiple records in single sheet

--- src/main/java/org/example/controllers/EmployeeAPI.java
+++ src/main/java/org/example/controllers/EmployeeAPI.java
@@ -1,18 +1,17 @@
 package org.example.controllers;
 
-import org.example.model.Employee;
 import org.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -21,29 +20,11 @@ public class EmployeeAPI {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
-        Optional<Employee> employee = employeeService.getEmployeeById(id);
-        return employee.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
-    public ResponseEntity<String> addEmployee(@RequestBody Employee employee) {
-        employeeService.addEmployee(employee);
-        return ResponseEntity.ok("Employee added successfully!");
-    }
-
     @GetMapping("/export")
     public ResponseEntity<InputStreamResource> exportEmployeesAsZip() throws IOException {
-        ByteArrayInputStream zipStream = employeeService.generateCsvAndZip();
+        ByteArrayInputStream zipStream = employeeService.generateCsvAndZipGroupedByDoj();
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Disposition", "attachment; filename=employees.zip");
+        headers.add("Content-Disposition", "attachment; filename=employees_by_doj.zip");
 
         return ResponseEntity.ok()
                 .headers(headers)
